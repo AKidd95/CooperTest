f4011678cc88072f5960ab992a9b10436ad8a247
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-02 16:55:26 +0100

    added cooper specs

--- spec/cooperSpec.js
+++ spec/cooperSpec.js
@@ -1,1 +1,14 @@
-describe ('Cooper')
+describe("Cooper", function() {
+  var assistant;
+  var track;
+
+  /* beforeEach(function() {
+    assistant = new Assistant({stopwatch: 12, whistle: true});
+    track = new Track({distance: 400});
+  }); */
+
+  it("athelete warms up for 10 minutes", function() {
+    expect(warm_up).toEqual(true);
+  });
+
+});
---
+++ src/cooper.js
@@ -0,0 +1,8 @@
+function Cooper() {};
+
+var warm_up = 10;
+if (warm_up = 10) {
+  warm_up = true
+} else {
+  warm_up = false
+};
