e4928215d2394874b7183dd754c9eeb5060cad57
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-03 12:28:10 +0100

    added person spec files

--- spec/personSpec.js
+++ spec/personSpec.js
@@ -1,59 +1,18 @@
-describe("person", function() {
-  var gender;
-  var age;
-
+describe('Person', function() {
   beforeEach(function() {
-    person = new Person();
-  });
-}
-
-  /* it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
-  });
-
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
-    });
-
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
-
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
+    person = new Person({
+      gender: 'Female',
+      age: 25
     });
   });
 
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  it('person should have gender', function() {
+    // expect(person.gender).toBeDefined();
+    expect(person.gender).toBe('Female');
   });
 
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
-    });
+  it('should have age', function() {
+    expect(person.age).toBe(25);
   });
+
 });
-*/
--- src/person.js
+++ src/person.js
@@ -1,4 +1,4 @@
-function person(attr) {
+function Person(attr) {
   this.gender = attr.gender;
   this.age = attr.age;
 }
