e1e4ce4b41a4162a72340f640b6fa8840ace5b11
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-04 16:52:19 +0100

    changed "assessment" for this.cooperMessage"

--- src/cooper.js
+++ src/cooper.js
@@ -2,15 +2,15 @@ function cooperAssess(this, distance) {
   if(this.gender === 'female') {
       if (this.age >= 13 && this.age <= 14 ) {
         if(distance > 2000) {
-          assessment = 'Excellent';
+          this.cooperMessage = 'Excellent';
         } else if (distance <= 1999 && distance >= 1900) {
-          assessment = 'Above average';
+          this.cooperMessage = 'Above average';
         } else if (distance <= 1899 && distance >= 1600) {
-          assessment = 'Average';
+          this.cooperMessage = 'Average';
         } else if (distance <= 1599 && distance >= 1500) {
-          assessment = 'Below Average';
+          this.cooperMessage = 'Below Average';
         } else if (distance < 1500) {
-          assessment = 'Poor';
+          this.cooperMessage = 'Poor';
         } else {
 
         }
@@ -18,85 +18,85 @@ function cooperAssess(this, distance) {
 
       if (this.age >= 15 && this.age <= 16 ) {
         if(distance > 2100) {
-          assessment = 'Excellent';
+          this.cooperMessage = 'Excellent';
         } else if (distance <= 2099 && distance >= 2000) {
-          assessment = 'Above average';
+          this.cooperMessage = 'Above average';
         } else if (distance <= 1999 && distance >= 1700) {
-          assessment = 'Average';
+          this.cooperMessage = 'Average';
         } else if (distance <= 1699 && distance >= 1600) {
-          assessment = 'Below Average';
+          this.cooperMessage = 'Below Average';
         } else (distance < 1600) {
-          assessment = 'Poor';
+          this.cooperMessage = 'Poor';
         }
       }
 
       if (this.age >= 17 && this.age <= 19 ) {
         if(distance > 2300) {
-          assessment = 'Excellent';
+          this.cooperMessage = 'Excellent';
         } else if (distance <= 2299 && distance >= 2100) {
-          assessment = 'Above average';
+          this.cooperMessage = 'Above average';
         } else if (distance <= 2099 && distance >= 1800) {
-          assessment = 'Average';
+          this.cooperMessage = 'Average';
         } else if (distance <= 1799 && distance >= 1700) {
-          assessment = 'Below Average';
+          this.cooperMessage = 'Below Average';
         } else (distance < 1700) {
-          assessment = 'Poor';
+          this.cooperMessage = 'Poor';
         }
       }
 
       if (this.age >= 20 && this.age <= 29 ) {
         if(distance > 2700) {
-          assessment = 'Excellent';
+          this.cooperMessage = 'Excellent';
         } else if (distance <= 2699 && distance >= 2200) {
-          assessment = 'Above average';
+          this.cooperMessage = 'Above average';
         } else if (distance <= 2199 && distance >= 1800) {
           this.cooperMessage = 'Average';
         } else if (distance <= 1799 && distance >= 1500) {
-          assessment = 'Below Average';
+          this.cooperMessage = 'Below Average';
         } else (distance < 1500) {
-          assessment = 'Poor';
+          this.cooperMessage = 'Poor';
         }
       }
 
       if (this.age >= 30 && this.age <= 39 ) {
         if(distance > 2500) {
-          assessment = 'Excellent';
+          this.cooperMessage = 'Excellent';
         } else if (distance <= 2499 && distance >= 2000) {
-          assessment = 'Above average';
+          this.cooperMessage = 'Above average';
         } else if (distance <= 1999 && distance >= 1700) {
-          assessment = 'Average';
+          this.cooperMessage = 'Average';
         } else if (distance <= 1699 && distance >= 1400) {
-          assessment = 'Below Average';
+          this.cooperMessage = 'Below Average';
         } else (distance < 1400) {
-          assessment = 'Poor';
+          this.cooperMessage = 'Poor';
         }
       }
 
       if (this.age >= 40 && this.age <= 49 ) {
         if(distance > 2300) {
-          assessment = 'Excellent';
+          this.cooperMessage = 'Excellent';
         } else if (distance <= 2299 && distance >= 1900) {
-          assessment = 'Above average';
+          this.cooperMessage = 'Above average';
         } else if (distance <= 1899 && distance >= 1500) {
-          assessment = 'Average';
+          this.cooperMessage = 'Average';
         } else if (distance <= 1499 && distance >= 1200) {
-          assessment = 'Below Average';
+          this.cooperMessage = 'Below Average';
         } else (distance < 1200) {
-          assessment = 'Poor';
+          this.cooperMessage = 'Poor';
         }
       }
 
       if (this.age >= 50 ) {
         if(distance > 2200) {
-          assessment = 'Excellent';
+          this.cooperMessage = 'Excellent';
         } else if (distance <= 2199 && distance >= 1700) {
-          assessment = 'Above average';
+          this.cooperMessage = 'Above average';
         } else if (distance <= 1699 && distance >= 1400) {
-          assessment = 'Average';
+          this.cooperMessage = 'Average';
         } else if (distance <= 1399 && distance >= 1100) {
-          assessment = 'Below Average';
+          this.cooperMessage = 'Below Average';
         } else (distance < 1100) {
-          assessment = 'Poor';
+          this.cooperMessage = 'Poor';
         }
       }
     }
